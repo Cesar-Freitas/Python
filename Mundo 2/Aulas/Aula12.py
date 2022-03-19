b7b5585a12b6a34650ff956a396baacd382fca00
César Augusto Freitas Bernardes <70416108+Cesar-Freitas@users.noreply.github.com>
2022-03-18 23:23:14 -0300

    Create Aula12.py

---
+++ Mundo 2/Aulas/Aula12.py
@@ -0,0 +1,10 @@
+nome = str(input("Qual é seu nome? "))
+if nome == 'César':
+    print("Que nome lindo!!")
+elif nome == "Enzo":
+    print('Caralho mlk')
+elif nome in "Paula Bruna Marcela":
+    print("Olha que coisa mais linda e mais cheia de graça...")
+else:
+    print("Nomezinho xexelento viu..")
+print("Tenha um bom dia {}.".format(nome))
